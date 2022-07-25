f3e842ef6cecc6b68ece123e4402916c0714cab9
lyf-is-coding <coi12tt34@gmail.com>
2022-07-25 12:54:13 +0700

    Update Example.cpp

--- Example.cpp
+++ Example.cpp
@@ -1,1 +1,13 @@
-Memory Mem(ProcessID);
+// Get Process Handle
+HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, ProcessID);
+
+// Read memory
+int buffer;
+if (!MM::read(hProcess, 0x006FF748, &buffer))
+    return 1;
+std::cout << buffer << '\n';
+
+// Write memory
+int buffer = -100;
+if (!MM::write(hProcess, 0x006FF748, &buffer))
+    return 1;
